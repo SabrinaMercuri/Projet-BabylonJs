8a3295f1bbff6ba7e7fbb6e840b01ee262bb28eb
Sabrina <sabrina.mercuri@etu.unice.fr>
2021-04-25 14:44:22 +0200

    Ajout des ennemis du niveau 2 Co-authored-by: Alphailosia

--- json/ennemi.js
+++ json/ennemi.js
@@ -22,6 +22,30 @@ export default
         {
             pos : { x:80,z: 380},
             angle: -90
+        },
+        {
+            pos : { x:180,z: 180},
+            angle: -90
+        },
+        {
+            pos : { x:60,z: 40},
+            angle: -90
+        },
+        {
+            pos : { x:-120,z: -20},
+            angle: -90
+        },
+        {
+            pos : { x:-200,z: 100},
+            angle: -90
+        },
+        {
+            pos : { x:-300,z: -35},
+            angle: -90
+        },
+        {
+            pos : { x:0,z: 160},
+            angle: -90
         }
     ]    
 ]
